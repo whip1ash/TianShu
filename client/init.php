6d18c4024984f0c4c40354d3aeca0b0d613c2337
whip1ash <usergeng@gmail.comh>
2017-11-27 18:33:34 +0800

    demo

--- client/init.php
+++ client/init.php
@@ -5,12 +5,13 @@
  * Date: 25/11/2017
  * Time: 20:24
  */
-define('init_correct',ture);
+define('init_correct',true);
 include_once ('includes/Request.php');
 $test_json = ['id'=>1];
 $test_json = json_encode($test_json);
-
-$url= 'www.baidu.com';
+echo $test_json;
+echo '<br>';
+$url= '123.206.83.137/server/controller/SortMessage.php';
 $test_post = Request::post($url,$test_json,1);
 echo $test_post;
 
--- server/controller/SortMessage.php
+++ server/controller/SortMessage.php
@@ -5,7 +5,7 @@
  * Date: 2017/11/25
  * Time: 19:56
  */
-   $post = $_POST ;
+   $post = json_decode($_POST);
    if (!empty($post['id'])){
        $return_data = array('success'=>true,'data'=>'test_success!','error'=>null);
        echo json_encode($return_data);
