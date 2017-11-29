ddf0513368aba01c6dba59b0c11d55f3d8ebdc62
Shi Xinlu <shixl@wingspan.cn>
2017-11-29 21:14:12 +0800

    修改SortMessage

--- server/controller/SortMessage.php
+++ server/controller/SortMessage.php
@@ -5,11 +5,33 @@
  * Date: 2017/11/25
  * Time: 19:56
  */
-   $data = json_decode($_POST['data'],1);
-   echo "<br>";
-   var_dump($data);
-   if (!empty($data['id'])){
-       $return_data = array('success'=>true,'data'=>'test_success!','error'=>null);
-       echo json_encode($return_data);
-   }
+include_once ("../DAO/MysqlData.php");
+
+try {
+    $data = json_decode($_POST['data'], 1);
+    $db = new Save();
+
+    $result = $db->saveClient($data);
+    if ($result) {
+        Common::save_http_log('[info]', '调用saveClient($client) 成功');
+        return json_encode(['success' => 'true']);
+    } else {
+        Common::save_http_log('[error]', '调用saveClient($client) 失败');
+        return json_encode(['error' => '服务器内部错误']);
+    }
+    $result = $db->saveHeartBeat($data);
+    if ($result) {
+        Common::save_http_log('[info]', '调用saveHeartBeat($client) 成功');
+        return json_encode(['success' => 'true']);
+    } else {
+        Common::save_http_log('[error]', '调用saveHeartBeat($client) 失败');
+        return json_encode(['error' => '服务器内部错误']);
+    }
+} catch (Exception $e) {
+    Common::save_http_log('[error]', 'SortMessage.php 出现异常');
+    return json_encode(['error' => '服务器内部错误']);
+}
+
+
+
 ?>
